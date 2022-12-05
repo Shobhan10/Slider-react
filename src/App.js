70ed36f90a17b9d1f022baed88f4c74115251011
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-12-05 23:37:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import people from "./data";
 
 function App() {
+  const [reviews, setReviews] = useState(people);
+  const [index, setIndex] = useState(0);
+
+  const { id, image, name, title, quote } = reviews[index];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>
+          Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        <article className="">
+          <img src={image} alt={name} className="person-img" />
+          <h4>{name}</h4>
+          <p className="title">{title}</p>
+          <p className="text">{quote}</p>
+          <FaQuoteRight className="icon" />
+        </article>
+        <button
+          className="prev"
+          onClick={() => {
+            index === 0 ? setIndex(reviews.length - 1) : setIndex(index - 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <FiChevronLeft />
+        </button>
+        <button
+          className="next"
+          onClick={() => {
+            index === reviews.length - 1 ? setIndex(0) : setIndex(index + 1);
+          }}
+        >
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
   );
 }
 
