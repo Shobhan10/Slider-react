cbe31f87fff80710fd5f16dccdb96a9226c068b8
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-12-06 07:24:20 +0530

    added all functionalities

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import people from "./data";
@@ -7,7 +7,12 @@ function App() {
   const [reviews, setReviews] = useState(people);
   const [index, setIndex] = useState(0);
 
-  const { id, image, name, title, quote } = reviews[index];
+  useEffect(() => {
+    const slider = setInterval(() => {
+      index === reviews.length - 1 ? setIndex(0) : setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
 
   return (
     <section className="section">
@@ -18,13 +23,32 @@ function App() {
         </h2>
       </div>
       <div className="section-center">
-        <article className="">
-          <img src={image} alt={name} className="person-img" />
-          <h4>{name}</h4>
-          <p className="title">{title}</p>
-          <p className="text">{quote}</p>
-          <FaQuoteRight className="icon" />
-        </article>
+        {reviews.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = "nextSlide";
+
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === reviews.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article key={id} className={position}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
         <button
           className="prev"
           onClick={() => {
